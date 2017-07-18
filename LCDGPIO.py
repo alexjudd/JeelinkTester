3d8cc1354ac99f0cce19c32553b1a0ebb405fe09
= <alexjudd79@gmail.com>
2017-07-18 11:15:42 +0100

    Added LCD message functionality to main script

--- LCDGPIO.py
+++ LCDGPIO.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python
+#!/usr/bin/python
 #--------------------------------------
 #    ___  ___  _ ____
 #   / _ \/ _ \(_) __/__  __ __
@@ -75,129 +75,116 @@ E_PULSE = 0.0005
 E_DELAY = 0.0005
 
 def main():
-  # Main program block
-  
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-  GPIO.setup(LCD_E, GPIO.OUT)  # E
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
-  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+    # Main program block
+    # Initialise display
+    lcd_init()
 
+    while True:
+        # Send some test
+        lcd_string("Some lovely text",LCD_LINE_1)
+        lcd_string("16x2 LCD",LCD_LINE_2)
 
-  # Initialise display
-  lcd_init()
+        time.sleep(3) # 3 second delay
+        # Send some text
+        lcd_string("Python works well",LCD_LINE_1)
+        lcd_string("quite well",LCD_LINE_2)
 
-  while True:
-
-    # Send some test
-    lcd_string("Some lovely text",LCD_LINE_1)
-    lcd_string("16x2 LCD",LCD_LINE_2)
-
-    time.sleep(3) # 3 second delay
-
-    # Send some text
-   # lcd_string("1234567890123456",LCD_LINE_1)
-    #lcd_string("abcdefghijklmnop",LCD_LINE_2)
-
-
-
-    #time.sleep(3) # 3 second delay
-
-    # Send some text
-    #lcd_string("RaspberryPi-spy",LCD_LINE_1)
-    #lcd_string(".co.uk",LCD_LINE_2)
-
-    #time.sleep(3)
-
-    # Send some text
-    lcd_string("Python works well",LCD_LINE_1)
-    lcd_string("quite well",LCD_LINE_2)
-
-    time.sleep(3)
+        time.sleep(3)
 
 def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
+    
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+    GPIO.setup(LCD_E, GPIO.OUT)  # E
+    GPIO.setup(LCD_RS, GPIO.OUT) # RS
+    GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+    GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+    GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+    GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+    # Initialise display
+    lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+    lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+    lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+    lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+    lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+    lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+    time.sleep(E_DELAY)
 
 def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = data
-  # mode = True  for character
-  #        False for command
+    # Send byte to data pins
+    # bits = data
+    # mode = True  for character
+    #        False for command
+    
+    GPIO.output(LCD_RS, mode) # RS
 
-  GPIO.output(LCD_RS, mode) # RS
+    # High bits
+    GPIO.output(LCD_D4, False)
+    GPIO.output(LCD_D5, False)
+    GPIO.output(LCD_D6, False)
+    GPIO.output(LCD_D7, False)
+    if bits&0x10==0x10:
+        GPIO.output(LCD_D4, True)
+    if bits&0x20==0x20:
+        GPIO.output(LCD_D5, True)
+    if bits&0x40==0x40:
+        GPIO.output(LCD_D6, True)
+    if bits&0x80==0x80:
+        GPIO.output(LCD_D7, True)
 
-  # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
+    # Toggle 'Enable' pin
+    lcd_toggle_enable()
 
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
+    # Low bits
+    GPIO.output(LCD_D4, False)
+    GPIO.output(LCD_D5, False)
+    GPIO.output(LCD_D6, False)
+    GPIO.output(LCD_D7, False)
+    if bits&0x01==0x01:
+        GPIO.output(LCD_D4, True)
+    if bits&0x02==0x02:
+        GPIO.output(LCD_D5, True)
+    if bits&0x04==0x04:
+        GPIO.output(LCD_D6, True)
+    if bits&0x08==0x08:
+        GPIO.output(LCD_D7, True)
 
-  # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
+    # Toggle 'Enable' pin
+    lcd_toggle_enable()
 
 def lcd_toggle_enable():
-  # Toggle enable
-  time.sleep(E_DELAY)
-  GPIO.output(LCD_E, True)
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)
-  time.sleep(E_DELAY)
+    # Toggle enable
+    time.sleep(E_DELAY)
+    GPIO.output(LCD_E, True)
+    time.sleep(E_PULSE)
+    GPIO.output(LCD_E, False)
+    time.sleep(E_DELAY)
 
 def lcd_string(message,line):
-  # Send string to display
+    # Send string to display
+    message = message.ljust(LCD_WIDTH," ")
 
+    lcd_byte(line, LCD_CMD)
 
+    for i in range(LCD_WIDTH):
+        lcd_byte(ord(message[i]),LCD_CHR)
 
+def displayMessage(message, line):
+    if line == 1:
+        lcd_string(message, LCD_LINE_1)
+    else:
+        lcd_string(message, LCD_LINE_2)
 
-  message = message.ljust(LCD_WIDTH," ")
-
-  lcd_byte(line, LCD_CMD)
-
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
+def cleanup():
+    GPIO.cleanup()
 
 if __name__ == '__main__':
 
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    lcd_byte(0x01, LCD_CMD)
-    lcd_string("Goodbye!",LCD_LINE_1)
-    GPIO.cleanup()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        lcd_byte(0x01, LCD_CMD)
+        lcd_string("Goodbye!",LCD_LINE_1)
+        cleanup()
--- usbconnect.py
+++ usbconnect.py
@@ -7,176 +7,176 @@ import argparse
 import sys
 from pyudev import Context, Monitor, MonitorObserver
 
+import LCDGPIO as lcd
+
 #define JeeDevice class
 class JeeDevice:
-	port = ''
-	shortId = ''
-	nodeId = ''
-	group = ''
-	freq = ''
-	
-	def description(self):
-		desc = '{0}\t{1}\t{2}\t{3}\t{4}'.format(self.port, self.shortId, self.nodeId, self.group, self.freq)
-		return desc
-	
-	def desc(self):
-		return 'A i{0} g{1} {2} MHz q1'.format(self.nodeId, self.group, self.freq)
+    port = ''
+    shortId = ''
+    nodeId = ''
+    group = ''
+    freq = ''
 
-		
-		
-profuse = False				#for debug reporting
-remoteDevice = JeeDevice()	#remote node for testing packets over radio
-currentDevice = JeeDevice()	#device currently being tested
+    def description(self):
+        desc = '{0}\t{1}\t{2}\t{3}\t{4}'.format(self.port, self.shortId, self.nodeId, self.group, self.freq)
+        return desc
+
+    def desc(self):
+        return 'A i{0} g{1} {2} MHz q1'.format(self.nodeId, self.group, self.freq)
+
+profuse = False             #for debug reporting
+remoteDevice = JeeDevice()  #remote node for testing packets over radio
+currentDevice = JeeDevice() #device currently being tested
 
 def vprint(message, level):
-	if profuse:
-		print(message)
+    if profuse:
+        print(message)
 
 def configArgParser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-c', '--conf', help='specified configuration file')
-	parser.add_argument('-v', '--verbosity', help='increases the verbosity of output', action='store_true')
-	return parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--conf', help='specified configuration file')
+    parser.add_argument('-v', '--verbosity', help='increases the verbosity of output', action='store_true')
+    return parser
 
 def setConfiguration():
-	configurationFile = 'tester.conf'		#default configuration file
-	
-	parser = configArgParser()
-	args = parser.parse_args()
-	if args.conf:
-		print('configuration set')
-		configurationFile = args.conf
-	
-	print('using configuration file ', configurationFile)
-	
-	if args.verbosity:
-		global profuse
-		profuse = True
-		print('verbose output selected')
-		
+    configurationFile = 'tester.conf'   #default configuration file
 
-	Config=configparser.ConfigParser()
-	#print(Config)
-	try:
-		with open(configurationFile) as f:
-			Config.readfp(f)
-	except IOError:
-		print('specified config file not found: {0}'.format(configurationFile))
-		sys.exit(1)
-		
+    parser = configArgParser()
+    args = parser.parse_args()
+    if args.conf:
+        print('configuration set')
+        configurationFile = args.conf
+        
+    print('using configuration file ', configurationFile)
 
-	
-	section='JeelinkRemote'						#Jeelink remote settings
-	dict1 = {}
-	options = Config.options(section)
-	for option in options:
-		dict1[option] = Config.get(section, option)
-		
-	global remoteDevice
-	remoteDevice.nodeId = dict1['nodeid']
-	remoteDevice.group = dict1['group']
-	remoteDevice.freq = dict1['frequency']
-	print('REMOTE DEVICE:', remoteDevice.desc())
+    if args.verbosity:
+        global profuse
+        profuse = True
+        print('verbose output selected')
+
+    Config=configparser.ConfigParser()
+    try:
+        with open(configurationFile) as f:
+            Config.readfp(f)
+    except IOError:
+        print('specified config file not found: {0}'.format(configurationFile))
+        sys.exit(1)
+
+    section='JeelinkRemote' #Jeelink remote settings
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        dict1[option] = Config.get(section, option)
+
+    global remoteDevice
+    remoteDevice.nodeId = dict1['nodeid']
+    remoteDevice.group = dict1['group']
+    remoteDevice.freq = dict1['frequency']
+    print('REMOTE DEVICE:', remoteDevice.desc())
 
 def openSerialInterface(device):
-	
-	ser = serial.Serial(
-		port=device.device_node,
-		baudrate=57600,
-		parity=serial.PARITY_NONE,
-		stopbits=serial.STOPBITS_ONE,
-		timeout=1)
-	
-	deviceConfig = consumePreamble(device, ser)
-	regex = r'(^A i(\d+) g(\d+) @ (\d+) MHz q(\d+))'
-	match = re.search(regex, deviceConfig)
-	
-	global currentDevice
-	currentDevice.shortId = device['ID_SERIAL_SHORT']
-	currentDevice.port = device.device_node
-	currentDevice.nodeId = match.group(2)
-	currentDevice.group = match.group(3)
-	currentDevice.freq = match.group(4)
-	
-	print('New device with ID {1} found on {0} with configuration {2}'.format(currentDevice.port, currentDevice.shortId, currentDevice.desc()))
-	
-	return ser
+    ser = serial.Serial(port=device.device_node,baudrate=57600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,timeout=1)
+    deviceConfig = consumePreamble(device, ser)
+    regex = r'(^A i(\d+) g(\d+) @ (\d+) MHz q(\d+))'
+    match = re.search(regex, deviceConfig)
+
+    global currentDevice
+    currentDevice.shortId = device['ID_SERIAL_SHORT']
+    currentDevice.port = device.device_node
+    currentDevice.nodeId = match.group(2)
+    currentDevice.group = match.group(3)
+    currentDevice.freq = match.group(4)
+
+    print('New device with ID {1} found on {0} with configuration {2}'.format(currentDevice.port, currentDevice.shortId, currentDevice.desc()))
+    lcd.displayMessage('ID: ' + currentDevice.shortId, 2)
+    return ser
 	
 def consumePreamble(device, ser):
-	count = 0
-	regex = r'(^A i(\d+) g(\d+) @ (\d+) MHz q(\d+))'	#matches last line of preamble e.g. A i1 g208 @ 868 MHz q1
-	
-	while True:
-		count = count + 1
-		line=ser.readline().strip().decode('utf-8')
-		vprint(line, 1)
-		if re.search(regex,line):
-			match = re.search(regex,line)
-			vprint ('{0}'.format(line),1)
-			return line
-		elif count >= 50:
-			'device config not found'
+    count = 0
+    regex = r'(^A i(\d+) g(\d+) @ (\d+) MHz q(\d+))'	#matches last line of preamble e.g. A i1 g208 @ 868 MHz q1
+    
+    while True:
+        count = count + 1
+        line=ser.readline().strip().decode('utf-8')
+        vprint(line, 1)
+        if re.search(regex,line):
+            match = re.search(regex,line)
+            vprint ('{0}'.format(line),1)
+            return line
+        elif count >= 50:
+            print('device config not found')
 			
 def blinkLED(device, ser):
-	timeout = 0.1
-	
-	print('blinking LED...')
-	for i in range(5):
-		ser.write(b'1l')
-		time.sleep(timeout)
-		ser.read(ser.inWaiting())
-		ser.write(b'0l')
-		time.sleep(timeout)
-		ser.read(ser.inWaiting())
+    timeout = 0.1
+    message = 'blinking LED...'
+    print(message)
+    lcd.displayMessage(message, 1)
+    
+    for i in range(5):
+        ser.write(b'1l')
+        time.sleep(timeout)
+        ser.read(ser.inWaiting())
+        ser.write(b'0l')
+        time.sleep(timeout)
+        ser.read(ser.inWaiting())
+
+    message = 'blinking LED...'
 		
 def packetTest(device, ser):
-	print('testing packets..')
-	timeout = 0.1
-	numpacks = 5
-	regex = r'(OK)'
-	
-	#set up the device so it has the same group and ensure id's are different
-	if remoteDevice.group != currentDevice.group:
-		print('Groups don\'t match')
-	if remoteDevice.freq != currentDevice.freq:
-		print('Frequencies don\'t match')
-	if remoteDevice.nodeId == currentDevice.nodeId:
-		print('node Id clash')
+    print('testing packets..')
+    timeout = 0.1
+    numpacks = 5
+    regex = r'(OK)'
+    
+    #set up the device so it has the same group and ensure id's are different
+    if remoteDevice.group != currentDevice.group:
+        print('Groups don\'t match')
+    if remoteDevice.freq != currentDevice.freq:
+        print('Frequencies don\'t match')
+    if remoteDevice.nodeId == currentDevice.nodeId:
+        print('node Id clash')
 
-	oks = 0
-	for x in range(numpacks):
-		ser.write(b'0t')
-		time.sleep(timeout)
-		response = ser.read(ser.inWaiting()).decode('utf-8')
-		if re.search(regex, response):
-			oks = oks +1
-			vprint (' '.join(response.split()),1)
-		time.sleep(timeout)
-	if oks == numpacks:
-		print('PASSED ({0}/{1})'.format(oks,numpacks))
-	else:
-		print('FAILED ({0}/{1}) consider using -v for more info'.format(oks,numpacks))
+    oks = 0
+    for x in range(numpacks):
+        ser.write(b'0t')
+        time.sleep(timeout)
+        response = ser.read(ser.inWaiting()).decode('utf-8')
+        if re.search(regex, response):
+            oks = oks +1
+            vprint (' '.join(response.split()),1)
+            lcd.displayMessage('PKTS:{0}/{1}'.format(oks, numpacks), 1)
+        time.sleep(timeout)
+    if oks == numpacks:
+        print('PASSED ({0}/{1})'.format(oks,numpacks))
+        lcd.displayMessage('PKTS:{0}/{1}      OK'.format(oks, numpacks), 1)
+    else:
+        print('FAILED ({0}/{1}) consider using -v for more info'.format(oks,numpacks))
+        lcd.displayMessage('PKTS:{0}/{1}    FAIL'.format(oks, numpacks), 1)
 
 def device_event(action, device):
-	if action == 'add':
-		ser = openSerialInterface(device)
-		blinkLED(device, ser)
-		packetTest(device, ser)
-		ser.close()
+    if action == 'add':
+        lcd.displayMessage('JEELINK ADDED', 1)
+        ser = openSerialInterface(device)
+        blinkLED(device, ser)
+        packetTest(device, ser)
+        ser.close()
 
 def main():
-	setConfiguration()	#configure testing environment variables
-	context = Context()
-	monitor = Monitor.from_netlink(context)
-	monitor.filter_by(subsystem='tty')
-	observer = MonitorObserver(monitor, device_event)
-	observer.start()
-	
-	while True:
-		n = input("Jeelink Tester v 1.0 type 'stop' to end\n")
-		if n == 'stop':
-			observer.stop()
-			break
+    lcd.lcd_init()          #initialize lcd display in preparation for sending messages
+    setConfiguration()      #query configuration and set remote properties for testing against
+    
+    context = Context()
+    monitor = Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='tty')
+    observer = MonitorObserver(monitor, device_event)
+    observer.start()
+
+    while True:
+        n = input("Jeelink Tester v 1.0 type 'stop' to end\n")
+        if n == 'stop':
+            observer.stop()
+            lcd.cleanup()
+            break
 
 if __name__ == "__main__":
-	main()
+    main()
